e7229cb54a275b27e98debd92a22056ec7a4b38d
mrinal365 <40198111+mrinal365@users.noreply.github.com>
2020-02-26 23:48:06 +0530

    Exceptions and Logging project done

---
+++ Maven_Exceptions-Logging/Maven/src/main/java/epam_cvrce/Maven_Exceptions_Logging/App.java
@@ -0,0 +1,13 @@
+package epam_cvrce.Maven_Exceptions_Logging;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Maven_Exceptions-Logging/Maven/src/main/java/epam_cvrce/Maven_Exceptions_Logging/SimpleCompoundInterest.java
@@ -0,0 +1,6 @@
+package com.maven_task.maven_task;
+
+public interface SimpleCompoundInterest {
+
+	double calculateInterest();
+}
